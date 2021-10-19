67d500fec7a10f7d4b161f988d4cfe6df5d2f435
ErlingMMM <71938112+ErlingMMM@users.noreply.github.com>
2021-10-19 20:04:24 +0200

    Person dao should list people by last name (passed)

--- src/main/java/no/kristiania/PersonDao.java
+++ src/main/java/no/kristiania/PersonDao.java
@@ -2,6 +2,7 @@ package no.kristiania;
 
 import javax.sql.DataSource;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PersonDao {
@@ -54,7 +55,30 @@ public class PersonDao {
         }
     }
 
-    public List<Person> listByLastName(String lastName) {
-        return null;
+    public List<Person> listByLastName(String lastName) throws SQLException {
+        try (Connection connection = dataSource.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement(
+                    "select * from people where las_name = ?"
+            )) {
+                statement.setString(1, lastName);
+
+                try (ResultSet rs = statement.executeQuery()) {
+                    ArrayList<Person> people = new ArrayList<>();
+                    rs.next();
+
+                    while (rs.next()) {
+                        Person person = new Person();
+                        person.setId(rs.getLong("id"));
+                        person.setFirstName(rs.getString("first_name"));
+                        person.setLastName(rs.getString("last_name"));
+                        people.add(person);
+                    }
+
+
+                    return people;
+                }
+            }
+        }
+
     }
 }
